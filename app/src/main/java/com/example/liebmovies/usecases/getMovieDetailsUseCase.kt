211255c69e2da44edeeba021533a9a6e60d53549
Omar Ahmed <osarious2@gmail.com>
2022-12-22 11:43:21 +0200

    removed unused folder

--- app/src/main/java/com/example/liebmovies/usecases/getMovieDetailsUseCase.kt
+++
@@ -1,5 +0,0 @@
-package com.example.liebmovies.usecases
-
-class getMovieDetailsUseCase {
-    //todo
-}
--- app/src/main/java/com/example/liebmovies/usecases/getMoviesUseCase.kt
+++
@@ -1,6 +0,0 @@
-package com.example.liebmovies.usecases
-
-class getMoviesUseCase {
-    //todo , this is in order to prevent usage if application context in viewModel,
-    // and instead inject the usecases in the view model for every user action
-}
