bf8248b6da8bd7e327040db08286c0762b3dbccb
Omar Ahmed <osarious2@gmail.com>
2023-01-05 07:07:38 +0200

    - made sure the the back stack goes correctly by adding the SingleLiveEvent for the movie details

--- app/src/main/java/com/example/liebmovies/fragments/MovieDetailsFragment.kt
+++ app/src/main/java/com/example/liebmovies/fragments/MovieDetailsFragment.kt
@@ -39,6 +39,8 @@ class MovieDetailsFragment : Fragment() {
         }
     }
 
+
+
     private fun renderMovieDetails(movieDetails: MyMovieDetails?) {
         binding.movieTitle.text = movieDetails?.title
         binding.movieType.text = movieDetails?.type
